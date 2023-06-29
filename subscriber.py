2bef633cf1069ca912953c20d1c92ed72f75bce6
srivyshnavikonijeti <97581418+srivyshnavikonijeti@users.noreply.github.com>
2023-06-29 15:25:54 +0530

    Add files via upload

---
+++ subscriber.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+
+import rospy
+import time
+from std_msgs.msg import String
+
+def callback(msg):
+	print(msg.data)
+
+rospy.init_node('listener',anonymous=True)
+sub=rospy.Subscriber('talker',String,callback)
+rospy.spin()
+
